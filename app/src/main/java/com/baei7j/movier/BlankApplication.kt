4650a8c92bf5c0fcc47816c1b1a3f867e00d395d
gemasnap <szabolcs.gema@snapsoft.hu>
2021-04-10 16:05:33 +0200

    feat: add rainbowcake

---
+++ app/src/main/java/com/baei7j/movier/BlankApplication.kt
@@ -0,0 +1,34 @@
+package com.baei7j.movier
+
+import co.zsmb.rainbowcake.config.Loggers
+import co.zsmb.rainbowcake.config.rainbowCake
+import co.zsmb.rainbowcake.dagger.BuildConfig
+import co.zsmb.rainbowcake.dagger.RainbowCakeApplication
+import co.zsmb.rainbowcake.timber.TIMBER
+import com.baei7j.movier.di.AppComponent
+import com.baei7j.movier.di.ApplicationModule
+import com.baei7j.movier.di.DaggerAppComponent
+import timber.log.Timber
+
+class BlankApplication : RainbowCakeApplication() {
+
+    override lateinit var injector: AppComponent
+
+    override fun setupInjector() {
+        injector = DaggerAppComponent.builder()
+            .applicationModule(ApplicationModule(this))
+            .build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        rainbowCake {
+            logger = Loggers.TIMBER
+            isDebug = BuildConfig.DEBUG
+        }
+
+        Timber.plant(Timber.DebugTree())
+    }
+
+}
