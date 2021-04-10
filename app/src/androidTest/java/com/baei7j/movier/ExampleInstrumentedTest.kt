4375e93c72e3d660cb3b3ab5a12d9de92d952ebd
gemasnap <szabolcs.gema@snapsoft.hu>
2021-04-10 16:10:40 +0200

    fix: Init unit test package name

--- app/src/androidTest/java/com/baei7j/movier/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/baei7j/movier/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.blank", appContext.packageName)
+        assertEquals("com.baei7j.movier", appContext.packageName)
     }
 }
